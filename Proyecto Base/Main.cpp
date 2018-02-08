75449acc932a25ca3b74eb695736ea0b7cb57674
Marçal <marcal.pujadas@epiagranollers.cat>
2018-02-08 18:27:36 +0100

    Molina parguela close #2

--- Proyecto Base/Main.cpp
+++ Proyecto Base/Main.cpp
@@ -13,16 +13,50 @@ float msFrame = 1 / (FPS / 1000.0f);
 
 SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer);
 
+SDL_Window* _gWindow = NULL;
+SDL_Renderer* _gRenderer = NULL;
 int main(int argc, char* args[]) {
 
-	
+	SDL_Init(SDL_INIT_VIDEO);
+	bool success = true;
+	//Create window
+	_gWindow = SDL_CreateWindow("MEGAMAN THE GAME", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (_gWindow == NULL)
+	{
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create renderer for window
+		_gRenderer = SDL_CreateRenderer(_gWindow, -1, SDL_RENDERER_ACCELERATED);
+		if (_gRenderer == NULL)
+		{
+			printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Initialize renderer color
+			SDL_SetRenderDrawColor(_gRenderer, 0xFF, 0xAA, 0xFF, 0xFF);
 
+			//Initialize PNG loading
+			int imgFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imgFlags) & imgFlags))
+			{
+				printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+				success = false;
+			}
+		}
+	}
+
+	
+	system("pause");
 	return 0;
 }
 
 SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer)
 {
-	//The final texture
 	SDL_Texture* newTexture = NULL;
 
 	//Load image at specified path
@@ -34,7 +68,7 @@ SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer)
 	else
 	{
 		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+		newTexture = SDL_CreateTextureFromSurface(_gRenderer, loadedSurface);
 		if (newTexture == NULL)
 		{
 			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
@@ -43,6 +77,7 @@ SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer)
 		//Get rid of old loaded surface
 		SDL_FreeSurface(loadedSurface);
 	}
+
 
 	return newTexture;
 }
